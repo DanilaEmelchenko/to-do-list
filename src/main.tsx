f43b2560691e1b5fbd3d25fd73a2d4fb9657f2e1
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-02-11 15:18:49 +0300

    Added CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <ScopedCssBaseline>
+    <App />
+  </ScopedCssBaseline>
+);
