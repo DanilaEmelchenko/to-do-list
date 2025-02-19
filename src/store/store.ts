7357a68399ccc5ab75d073f2b6b9fe5403aa9f14
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-02-19 12:17:05 +0300

    name reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "./usersSlice/usersSlice";
+import usersList from "./usersSlice/usersSlice";
 import { useDispatch, useSelector } from "react-redux";
-import postsReducer from "./postsSlice/postsSlice";
+import postsList from "./postsSlice/postsSlice";
 
 export const store = configureStore({
   reducer: {
-    usersReducer: usersReducer,
-    postsReducer: postsReducer,
+    users: usersList,
+    posts: postsList,
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
