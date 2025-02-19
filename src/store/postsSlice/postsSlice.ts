3751b005a6477364c2255aa2b4007ced42a51711
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-02-19 12:15:04 +0300

    cod styles interface

--- src/store/postsSlice/postsSlice.ts
+++ src/store/postsSlice/postsSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { IPosts, Posts } from "../../services/posts";
+import { IPostsResponse, Posts } from "../../services/posts";
 
 export const fetchPosts = createAsyncThunk(
   "posts/fetchPosts",
@@ -9,8 +9,8 @@ export const fetchPosts = createAsyncThunk(
   }
 );
 
-interface PostsState {
-  posts: IPosts[];
+interface IPostsState {
+  posts: IPostsResponse[];
   loading: boolean;
   err: string | null;
 }
@@ -19,34 +19,26 @@ const initialState = {
   posts: [],
   loading: false,
   err: null,
-} satisfies PostsState as PostsState;
+} satisfies IPostsState as IPostsState;
 
-// Then, handle actions in your reducers:
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {
-    // standard reducer logic, with auto-generated action types per reducer
-  },
+  reducers: {},
   extraReducers: (builder) => {
-    // Add reducers for additional action types here, and handle loading state as needed
     builder
       .addCase(fetchPosts.pending, (state) => {
-        // Add user to the state array
         state.loading = true;
       })
       .addCase(fetchPosts.fulfilled, (state, action) => {
-        // Add user to the state array
         state.posts = action.payload;
         state.loading = false;
       })
       .addCase(fetchPosts.rejected, (state) => {
-        // Add user to the state array
         state.loading = false;
         state.err = "Ошибка загрузки постов";
       });
   },
 });
 
-// Later, dispatch the thunk as needed in the app
 export default postsSlice.reducer;
