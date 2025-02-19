602a9890f274b3bb9df9779261c2c693a5241f91
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-02-19 10:37:45 +0300

    Function to get your own array of user object with data

---
+++ src/store/usersSlice/getUsers.ts
@@ -0,0 +1,21 @@
+import { IUserResponse } from "../../services/user";
+import { IUser } from "./interfaces";
+
+export const getUsers = (data: IUserResponse[]): IUser[] => {
+  return data.map((item) => {
+    const copyAddress = {
+      ...item.address,
+    };
+    const { geo, ...deleteGeo } = copyAddress;
+    const fullAddress = Object.values(deleteGeo).join(" ,");
+
+    const fullName = [item.name, item.username].join(" ");
+
+    return {
+      id: item.id,
+      fullName: fullName,
+      email: item.email,
+      fullAddress: fullAddress,
+    };
+  });
+};
