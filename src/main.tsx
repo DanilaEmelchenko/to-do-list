e93f9209a2d3e8671bcee1e732a9a7bd6bc2c3d2
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-02-19 10:42:13 +0300

    added AppRouter component for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import { BrowserRouter } from "react-router";
+import AppRouter from "./components/AppRouter.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <ScopedCssBaseline>
-    <App />
-  </ScopedCssBaseline>
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <ScopedCssBaseline>
+          <AppRouter />
+        </ScopedCssBaseline>
+      </Provider>
+    </BrowserRouter>
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file."
+  );
+}
