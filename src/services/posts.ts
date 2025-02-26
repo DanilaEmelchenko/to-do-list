cef0d5f3e814ead0197ceea2fbf988c3523d996a
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-02-26 12:16:08 +0300

    request to add a post

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -7,11 +7,32 @@ export interface IPostsResponse {
   body: string;
 }
 
+export interface ICreatePostArgs {
+  title: string;
+  body: string;
+  userId: number;
+}
+
+export interface ICreatePostResponse {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
 export class Posts {
   static async fetchPosts(userId: number): Promise<IPostsResponse[]> {
     const response = await instance.get<IPostsResponse[]>(
       `/posts?userId=${userId}`
     );
+
+    return response.data;
+  }
+
+  static async creatingPost(
+    args: ICreatePostArgs
+  ): Promise<ICreatePostResponse> {
+    const response = await instance.post<ICreatePostResponse>(`/posts`, args);
 
     return response.data;
   }
