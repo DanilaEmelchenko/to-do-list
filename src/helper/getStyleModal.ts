4055397ce74bf25c3501ec95c5bf2b62846b06a6
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-03-05 14:02:58 +0300

    function for style component Modal

---
+++ src/helper/getStyleModal.ts
@@ -0,0 +1,14 @@
+export const getStyleModal = () => {
+  return {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100%",
+    maxWidth: "400px",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+};
