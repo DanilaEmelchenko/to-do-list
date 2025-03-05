f01def13926913e7b8500f932be549c624f816ae
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-03-05 14:37:29 +0300

    creating functionality for editing and deleting posts

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,25 +1,70 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ModeIcon from "@mui/icons-material/Mode";
 import { useAppDispatch, useAppSelector } from "../store/store";
-import { fetchPosts } from "../store/postsSlice/postsSlice";
+import {
+  deletePost,
+  editPost,
+  fetchPosts,
+} from "../store/postsSlice/postsSlice";
 import Loading from "../components/Loading/Loading";
+import {
+  selectLoadingPosts,
+  selectPostsData,
+} from "../store/selectors/postsSelectors";
 import FormCreateNewPost from "../components/FormCreateNewPost/FormCreateNewPost";
-import { selectLoadingPosts, selectPostsData } from "../store/selectors/postsSelectors";
+import ModalEditPost from "../components/ModalEditPost/ModalEditPost";
 
 const Posts = () => {
   const dispatch = useAppDispatch();
   const posts = useAppSelector(selectPostsData);
-  const loading = useAppSelector(selectLoadingPosts)
+  const loading = useAppSelector(selectLoadingPosts);
   const { userId } = useParams();
+  const [editPostId, setEditPostId] = useState<number | null>(null);
+  const [editMessagePost, setEditMessagePost] = useState<string>("");
+  const [editTitlePost, setEditTitlePost] = useState<string>("");
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     dispatch(fetchPosts(Number(userId)));
   }, []);
+
+  const clickDeletePost = (id: number): void => {
+    dispatch(deletePost(id));
+  };
+
+  const clickEditPost = (
+    postId: number,
+    titlePost: string,
+    messagePost: string
+  ): void => {
+    setEditPostId(postId);
+    setEditTitlePost(titlePost);
+    setEditMessagePost(messagePost);
+    handelModalOpen();
+  };
+
+  const saveEditPost = (): void => {
+    dispatch(
+      editPost({
+        id: editPostId as number,
+        title: editTitlePost,
+        body: editMessagePost,
+        userId: Number(userId),
+      })
+    );
+    handelModalClose();
+  };
+
+  const handelModalOpen = () => setModalOpen(true);
+  const handelModalClose = () => setModalOpen(false);
 
   if (loading) return <Loading />;
 
@@ -30,8 +75,6 @@ const Posts = () => {
         sx={{
           margin: "0 auto",
           width: "100%",
-          maxWidth: 1440,
-          bgcolor: "background.paper",
         }}
       >
         {posts.map((post, index) => (
@@ -40,12 +83,35 @@ const Posts = () => {
               <ListItemAvatar>
                 <span>{index + 1}.</span>
               </ListItemAvatar>
-              <ListItemText primary={post.title} secondary={post.body} />
+              <>
+                <ListItemText primary={post.title} secondary={post.body} />
+                <IconButton
+                  aria-label="edit"
+                  onClick={() => clickEditPost(post.id, post.title, post.body)}
+                >
+                  <ModeIcon />
+                </IconButton>
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => clickDeletePost(post.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </>
             </ListItem>
             <Divider variant="inset" component="li" />
           </div>
         ))}
       </List>
+      <ModalEditPost
+        editMessagePost={editMessagePost}
+        editTitlePost={editTitlePost}
+        setEditMessagePost={setEditMessagePost}
+        setEditTitlePost={setEditTitlePost}
+        saveEditPost={saveEditPost}
+        handelModalClose={handelModalClose}
+        modalOpen={modalOpen}
+      />
       <FormCreateNewPost />
     </>
   );
